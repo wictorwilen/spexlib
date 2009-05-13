02f7bd2b7377b867009e78fcfa3e6eeebf084199
Wictor Wilén <wictorwilen@users.noreply.github.com>
2009-05-13 17:29:17 +0000

    XmlNode and XElement extensions

--- SPExLib/SPExLib/GeneralExtensions/XmlNodeExtensions.cs
+++ SPExLib/SPExLib/GeneralExtensions/XmlNodeExtensions.cs
@@ -13,25 +13,19 @@
  * http://www.opensource.org/licenses/ms-pl.html
  * 
  */
-using System;
-using System.Xml;
+
 using System.Xml.Linq;
+using System.Xml;
 
 namespace SPExLib.Extensions {
-    public static class XElementExtensions {
-      
-        
+    public static class XmlNodeExtensions {
         // Kudos to Eric White: http://blogs.msdn.com/ericwhite/archive/2008/12/22/convert-xelement-to-xmlnode-and-convert-xmlnode-to-xelement.aspx
-        public static XmlNode GetXmlNode(this XElement element) {
-            using (XmlReader xmlReader = element.CreateReader()) {
-                XmlDocument xmlDoc = new XmlDocument();
-                xmlDoc.Load(xmlReader);
-                return xmlDoc;
-            }
-        }
+        public static XElement GetXElement(this XmlNode node) {
+            XDocument xDoc = new XDocument();
+            using (XmlWriter xmlWriter = xDoc.CreateWriter())
+                node.WriteTo(xmlWriter);
+            return xDoc.Root;
 
-        public static XmlElement GetXmlElement(this XElement element) {
-            return GetXmlNode(element).FirstChild as XmlElement;
         }
     }
 }
