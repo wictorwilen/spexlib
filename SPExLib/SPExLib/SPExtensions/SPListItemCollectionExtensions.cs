11a91130035c08799c19e6daa2d8e1e82f3b9410
Wictor Wilén <wictorwilen@users.noreply.github.com>
2009-05-13 17:21:13 +0000

    SPListItemCollection extensions

---
+++ SPExLib/SPExLib/SPExtensions/SPListItemCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * SharePoint Extensions Library
+ * http://SPExLib.codeplex.com/
+ * 
+ * ------------------------------------------
+ * 
+ * by Wictor Wilén
+ * http://www.wictorwilen.se
+ * 
+ * ------------------------------------------
+ * 
+ * Licensed under the Microsoft Public License (Ms-PL) 
+ * http://www.opensource.org/licenses/ms-pl.html
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SharePoint;
+
+namespace SPExLib.SPExtensions {
+    public static class SPListItemCollectionExtensions {
+        public static List<SPListItem> FindByField(this SPListItemCollection input, string field, object value) {
+            return (from SPListItem item in input where item[field] == value select item).ToList();
+        }
+        public static List<SPListItem> FindByField(this SPListItemCollection input, Guid field, object value) {
+            return (from SPListItem item in input where item[field] == value select item).ToList();
+        }
+
+    }
+}
