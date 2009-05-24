ecd5d08379c03ef4cd3809d0c9277b9e27f76172
wictor_cp <SND\wictor_cp>
2009-05-24 12:27:19 +0000

    Inline documentation started

--- SPExLib/SPExLib/SharePoint/SPListExtensions.cs
+++ SPExLib/SPExLib/SharePoint/SPListExtensions.cs
@@ -20,10 +20,21 @@ using Microsoft.SharePoint;
 
 namespace SPExLib.SharePoint {
     public static class SPListExtensions {
+        /// <summary>
+        /// Checks if the specified view exists
+        /// </summary>
+        /// <param name="list">The SPList object</param>
+        /// <param name="title">Title of the list (case insensitive)</param>
+        /// <returns><c>True</c> if the view exists, otherwise <c>false</c></returns>
         public static bool ViewExists(this SPList list, string title) {
             return list.Views.Cast<SPView>().Any(view => string.Compare(view.Title, title, true) == 0);
         }
 
+        /// <summary>
+        /// Performs the <paramref name="action"/> on each item in the list
+        /// </summary>
+        /// <param name="source">The SPList object</param>
+        /// <param name="action">The action to perform on the item</param>
         public static void ForEach(this SPList source, Action<SPListItem> action) {
             source.Items.ForEach(action);
         }
--- SPExLib/SPExLib/SharePoint/SPListItemExtension.cs
+++ SPExLib/SPExLib/SharePoint/SPListItemExtension.cs
@@ -18,6 +18,12 @@ using Microsoft.SharePoint;
 
 namespace SPExLib.SharePoint {
     public static class SPListItemExtension {
+        /// <summary>
+        /// Returns the specified column value, typed as string, from a list item
+        /// </summary>
+        /// <param name="item">The SPListItem</param>
+        /// <param name="column">Name of the column</param>
+        /// <returns>A string value, if no data is found, or the column is not found,  <c>string.Empty</c> is returned</returns>
         public static string GetListItemString(this SPListItem item, string column) {
             object data = item.GetListItemData(column);
 
@@ -26,6 +32,12 @@ namespace SPExLib.SharePoint {
             else
                 return string.Empty;
         }
+        /// <summary>
+        /// Returns the specified column value, typed as DateTime, from a list item
+        /// </summary>
+        /// <param name="item">The SPListItem</param>
+        /// <param name="column">Name of the column</param>
+        /// <returns>A DateTime value, if no data is found, or the column is not found,  <c>1900-01-01</c> is returned</returns>
         public static DateTime GetListItemDateTime(this SPListItem item, string column) {
             object data = item.GetListItemData(column);
 
@@ -34,7 +46,12 @@ namespace SPExLib.SharePoint {
             else
                 return new DateTime(1900, 1, 1);
         }
-
+        /// <summary>
+        /// Returns the specified column value, typed as bool, from a list item
+        /// </summary>
+        /// <param name="item">The SPListItem</param>
+        /// <param name="column">Name of the column</param>
+        /// <returns>A bool value, if no data is found, or the column is not found, <c>False</c> is returned</returns>
         public static bool GetListItemBool(this SPListItem item, string column) {
             object data =item.GetListItemData(column);
 
@@ -43,7 +60,12 @@ namespace SPExLib.SharePoint {
             else
                 return false;
         }
-
+        /// <summary>
+        /// Returns the value of the specified column value
+        /// </summary>
+        /// <param name="item">The SPListItem</param>
+        /// <param name="column">Name of the column</param>
+        /// <returns>The value of the column, if the column is not found <c>Null</c> is returned</returns>
         public static object GetListItemData(this SPListItem item, string column) {
             if (item.Contains(column))
                 return item[column];
@@ -51,6 +73,12 @@ namespace SPExLib.SharePoint {
                 return null;
         }
 
+        /// <summary>
+        /// Checks if the list contains a field with the specified name
+        /// </summary>
+        /// <param name="item">The SPListItem item</param>
+        /// <param name="fieldName">The name of the Field</param>
+        /// <returns><c>True</c> if the field exits, otherwisr <c>False</c></returns>
         public static bool Contains(this SPListItem item, string fieldName) {
             return item.Fields.ContainsField(fieldName);
         }
