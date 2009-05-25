654a13f9b16cf7da6e65ab9df9fa4c630026c979
Keith Dahlby <dahlbyk@gmail.com>
2009-05-25 15:41:51 +0000

    Added SPExLib.SharePoint.Security namespace; added system impersonation extensions

---
+++ SPExLib/SPExLib/SharePoint/Security/SPSiteSecurityExtensions.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * SharePoint Extensions Library
+ * http://SPExLib.codeplex.com/
+ * 
+ * ------------------------------------------
+ * 
+ * Licensed under the Microsoft Public License (Ms-PL) 
+ * http://www.opensource.org/licenses/ms-pl.html
+ * 
+ */
+using System;
+using Microsoft.SharePoint;
+
+namespace SPExLib.SharePoint.Security
+{
+    /// <summary>
+    /// Provides extension methods to simplify <see cref="SPSite"/> impersonation for elevated privileges.
+    /// </summary>
+    /// <remarks>For more information, see http://solutionizing.net/2009/01/06/elegant-spsite-elevation/.</remarks>
+    public static class SPSiteImpersonationExtensions
+    {
+        public static SPUserToken GetSystemToken(this SPSite site)
+        {
+            SPUserToken token = null;
+            bool tempCADE = site.CatchAccessDeniedException;
+            try
+            {
+                site.CatchAccessDeniedException = false;
+                token = site.SystemAccount.UserToken;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                SPSecurity.RunWithElevatedPrivileges(() =>
+                {
+                    using (SPSite elevSite = new SPSite(site.ID))
+                        token = elevSite.SystemAccount.UserToken;
+                });
+            }
+            finally
+            {
+                site.CatchAccessDeniedException = tempCADE;
+            }
+            return token;
+        }
+
+        public static void RunAsSystem(this SPSite site, Action<SPSite> action)
+        {
+            using (var elevSite = new SPSite(site.ID, site.GetSystemToken()))
+                action(elevSite);
+        }
+
+        public static void RunAsSystem(this SPSite site, Guid webId, Action<SPWeb> action)
+        {
+            site.RunAsSystem(elevSite =>
+            {
+                using (var elevWeb = elevSite.OpenWeb(webId))
+                    action(elevWeb);
+            });
+        }
+
+        public static void RunAsSystem(this SPSite site, string url, Action<SPWeb> action)
+        {
+            site.RunAsSystem(elevSite =>
+            {
+                using (var elevWeb = elevSite.OpenWeb(url))
+                    action(elevWeb);
+            });
+        }
+
+        public static T SelectAsSystem<T>(this SPSite site, Func<SPSite, T> selector)
+        {
+            using (SPSite elevSite = new SPSite(site.ID, site.GetSystemToken()))
+                return selector(elevSite);
+        }
+
+        public static T SelectAsSystem<T>(this SPSite site, Guid webId, Func<SPWeb, T> selector)
+        {
+            return site.SelectAsSystem(elevSite =>
+            {
+                using (var elevWeb = elevSite.OpenWeb(webId))
+                    return selector(elevWeb);
+            });
+        }
+
+        public static T SelectAsSystem<T>(this SPSite site, string url, Func<SPWeb, T> selector)
+        {
+            return site.SelectAsSystem(elevSite =>
+            {
+                using (var elevWeb = elevSite.OpenWeb(url))
+                    return selector(elevWeb);
+            });
+        }
+    }
+}
---
+++ SPExLib/SPExLib/SharePoint/Security/SPWebSecurityExtensions.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * SharePoint Extensions Library
+ * http://SPExLib.codeplex.com/
+ * 
+ * ------------------------------------------
+ * 
+ * Licensed under the Microsoft Public License (Ms-PL) 
+ * http://www.opensource.org/licenses/ms-pl.html
+ * 
+ */
+using System;
+using Microsoft.SharePoint;
+
+namespace SPExLib.SharePoint.Security
+{
+    /// <summary>
+    /// Provides extension methods to simplify <see cref="SPWeb"/> impersonation for elevated privileges.
+    /// </summary>
+    /// <remarks>For more information, see http://solutionizing.net/2009/01/06/elegant-spsite-elevation/.</remarks>
+    public static class SPWebSecurityExtensions
+    {
+        public static void RunAsSystem(this SPWeb web, Action<SPWeb> action)
+        {
+            web.Site.RunAsSystem(web.ID, action);
+        }
+
+        public static T SelectAsSystem<T>(this SPWeb web, Func<SPWeb, T> selector)
+        {
+            return web.Site.SelectAsSystem(web.ID, selector);
+        }
+    }
+}
