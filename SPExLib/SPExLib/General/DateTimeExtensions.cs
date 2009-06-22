430ca2e8a9c58bc7ceb783557be1c8a626c94a3e
Wictor Wilén <wictorwilen@users.noreply.github.com>
2009-06-22 23:24:07 +0000

    Added: DateTime.ToSPQueryStringFormat()

--- SPExLib/SPExLib/General/DateTimeExtensions.cs
+++ SPExLib/SPExLib/General/DateTimeExtensions.cs
@@ -15,16 +15,34 @@
  */
 using System;
 using System.Globalization;
+using Microsoft.SharePoint.Utilities;
 
 namespace SPExLib.General
 {
     public static class DateTimeExtensions {
 
+        /// <summary>
+        /// Static Datetime method that parses a value for a DateTime? value. if the value is null then null is returned
+        /// </summary>
+        /// <param name="value">the object to parse</param>
+        /// <returns>A DateTime value or null</returns>
         public static DateTime? ParseNull(object value) {
             if (value == null) {
                 return null;
             }
             return DateTime.Parse(value.ToString(), CultureInfo.InvariantCulture);
         }
+
+
+        /// <summary>
+        /// Converts a DateTime object to a string format used in SPQuery CAML
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns>A date/time formatted for use in a SPQuery CAML query</returns>
+        /// <remarks>Contribution from http://mo.notono.us/</remarks>
+        public static string ToSPQueryStringFormat(this DateTime dateTime) {
+            return SPUtility.CreateISO8601DateTimeFromSystemDateTime(dateTime);
+        }
+
     }
 }
